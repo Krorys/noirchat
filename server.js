61e5031507afdbec04553449ce657d23ee327bdf
Krorys <mr.krorys@gmail.com>
2017-03-05 21:05:40 +0100

    on vire mongodb

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/test');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
 
 app.use("/", express.static(__dirname + "/public"));
 http.listen(3000, function() {
